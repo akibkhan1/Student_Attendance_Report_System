a5df00d97444ab34c8bc9d9ef7553190dba7844e
akibkhan1 <akibkhanrt@gmail.com>
2019-09-30 12:01:43 +0600

    This is the main project

--- Attendance_Report_System(main)/src/home_page.java
+++ Attendance_Report_System(main)/src/home_page.java
@@ -10,9 +10,10 @@ import java.awt.Color;
 import java.awt.Font;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import javax.swing.JLabel;
 
 
-public class Login {
+public class home_page {
 
 	private JFrame frame;
 
@@ -30,7 +31,7 @@ public class Login {
 						System.out.println("Error" + e);
 				}
 				try {
-					Login window = new Login();
+					home_page window = new home_page();
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -42,7 +43,7 @@ public class Login {
 	/**
 	 * Create the application.
 	 */
-	public Login() {
+	public home_page() {
 		initialize();
 	}
 
@@ -69,26 +70,41 @@ public class Login {
 		btnNewButton.setForeground(Color.WHITE);
 		btnNewButton.setBackground(new Color(0, 100, 0));
 		btnNewButton.setFont(new Font("Sitka Heading", Font.BOLD, 15));
-		btnNewButton.setBounds(403, 198, 106, 52);
+		btnNewButton.setBounds(403, 238, 106, 52);
 		frame.getContentPane().add(btnNewButton);
 		
 		JButton btnNewButton_1 = new JButton("Teacher");
 		btnNewButton_1.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+				frame.dispose();
+				Teacher ob= new Teacher();
+				ob.setVisible(true);
 			}
 		});
 		btnNewButton_1.setForeground(Color.WHITE);
 		btnNewButton_1.setBackground(new Color(0, 100, 0));
 		btnNewButton_1.setFont(new Font("Sitka Subheading", Font.BOLD, 16));
-		btnNewButton_1.setBounds(403, 335, 106, 52);
+		btnNewButton_1.setBounds(403, 360, 106, 52);
 		frame.getContentPane().add(btnNewButton_1);
 		
 		JButton btnNewButton_2 = new JButton("Admin");
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				frame.dispose();
+				Admin ob= new Admin();
+				ob.setVisible(true);
+			}
+		});
 		btnNewButton_2.setForeground(Color.WHITE);
 		btnNewButton_2.setBackground(new Color(0, 100, 0));
 		btnNewButton_2.setFont(new Font("Sitka Subheading", Font.BOLD, 16));
-		btnNewButton_2.setBounds(403, 462, 106, 52);
+		btnNewButton_2.setBounds(403, 485, 106, 52);
 		frame.getContentPane().add(btnNewButton_2);
+		
+		JLabel lblIutStudentsAttendace = new JLabel("IUT Students' Attendace Report System");
+		lblIutStudentsAttendace.setFont(new Font("Times New Roman", Font.BOLD, 35));
+		lblIutStudentsAttendace.setBounds(177, 94, 610, 41);
+		frame.getContentPane().add(lblIutStudentsAttendace);
 	}
 	public Color getFrameContentPaneBackground() {
 		return frame.getContentPane().getBackground();
